4827365433902385231a3b51658f653098876830
a-flyleaf <44915874+a-flyleaf@users.noreply.github.com>
2021-04-28 12:42:18 -0400

    code cleanup

---
+++ p5.js
@@ -0,0 +1,61 @@
+//creates canvas
+function setup(){
+    let myCanvas=createCanvas(500,500);
+    myCanvas.parent('snekbox');
+}
+
+//starting position
+let x = 50;
+let y = 50;
+
+//will be run continuously unless stopped
+function draw(){
+    //rgba background color
+        //must be set here (NOT css) or the square leaves a trail
+    background(32);
+
+    //setting boundaries
+    let leftWall=25;
+    let rightWall=475;
+    let topWall=25;
+    let bottomWall=475;
+        //this could probably be simplified to two variables but I'm keeping it separate to clarify wall-checking
+
+    //visible boundaries
+    fill(238);
+    noStroke();
+    rect(0,0,leftWall,height);
+    rect(0,0,width,topWall);
+    rect(rightWall,0,width,height);
+    rect(0,bottomWall,width,height);
+
+    //controls
+        //the -/+ signs HAVE to precede the = or it reads it like "x = -5" or "= 5"
+        //key specification also has to be caps lock, breaks if lowercase
+    if (keyIsDown(LEFT_ARROW)) {x -= 5;}
+    if (keyIsDown(RIGHT_ARROW)) {x+=5;}
+    if (keyIsDown(UP_ARROW)) {y-=5;}
+    if (keyIsDown(DOWN_ARROW)) {y+=5;}
+    
+    //checks if square hits walls
+    if (
+        (x <= leftWall) ||
+        (y <= topWall) ||
+        (x >= rightWall - 25) ||
+        (y >= bottomWall - 25)
+        //subtraction based on square dimensions, measured from top left
+    ) {
+        //turn square red
+        fill(255,0,0);
+
+        //lose messae
+        textAlign(RIGHT);
+        text('ya done hecked up',475,490);
+        
+        //stops all movement
+        noLoop();
+    }
+
+    //creates square
+    rect(x,y,25,25);
+}
